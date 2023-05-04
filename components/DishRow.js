ee92168da8eae4cd2269d92062e3c7af882bf34e
Uche Chris <chrisuche58@yahoo.com>
2023-05-04 13:38:39 +0100

    update

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,12 +1,29 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
 
-const DishRow = ({id, name, description, price, image}) => {
+import { formatCurrency } from "../utility/formatCurrency"
+
+
+
+const DishRow = ({id, name, description, price, image,imgUrl}) => {
   return (
     <TouchableOpacity>
       <View>
         <Text className="text-lg mb-1" >{name}</Text>
         <Text className="text-gray-400" >{description}</Text>
+        <Text className="text-gray-400 mt-2" >{formatCurrency(price)}
+        
+            </Text>
+      </View>
+      <View>
+        <Image 
+
+        source={{uri:imgUrl }}
+        style={{
+            borderWidth: 1,
+            borderColor: "#F3F3F4"
+        }}
+        className= "h-20 w-20 bg-gray-300 p-4"/>
       </View>
     </TouchableOpacity>
   );
