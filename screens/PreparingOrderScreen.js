57cd2bd27bfc6de76a297bb72b15ab36295ed1e0
Uche Chris <chrisuche58@yahoo.com>
2023-05-10 23:54:19 +0100

    update

--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -1,16 +1,27 @@
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView} from 'react-native';
 import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import * as Animatable from 'react-native-animatable';
+import * as Progress from 'react-native-progress';
 
 const PreparingOrderScreen = () => {
   return (
-    <SafeAreaView className="bg-[#00CCBB] flex-1 justify-center items-center">
+    <SafeAreaView className="bg-white flex-1 justify-center items-center">
       <Animatable.Image 
-        source={require("../assets/pay.gif")}
+        source={require("../assets/pay1.gif")}
         animation="slideInUp"
+        iterationCount={1}
         className="h-96 w-96"
         />
+
+        <Animatable.Text 
+            animation="slideInUp"
+            iterationCount={1}
+            className="text-lg my-10  text-center"
+        >
+        Waiting for Restaurant to accept your order!
+        </Animatable.Text>
+
+        <Progress.Circle size={60} indeterminate={true} color="gray"/>
     </SafeAreaView>
   );
 };
