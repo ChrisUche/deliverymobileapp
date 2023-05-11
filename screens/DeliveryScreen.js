a7c4978e181bc7f427a84d66cdb0e9c0d00e9439
Uche Chris <chrisuche58@yahoo.com>
2023-05-11 14:25:22 +0100

    xicon

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { useSelector } from 'react-redux';
+import { selectRestaurant } from '../features/restaurantSlice';
+import { XCircleIcon, XMarkIcon } from 'react-native-heroicons/solid';
+
 
 const DeliveryScreen = () => {
+    const navigation = useNavigation();
+    const restaurant = useSelector(selectRestaurant);
   return (
-    <View>
-      <Text>DeliveryScreen</Text>
+    <View className="bg-[#00CCBB] flex-1">
+      <SafeAreaView>
+        <View >
+            <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+                <XCircleIcon color="white" size={30}/>
+            </TouchableOpacity>
+            <Text>Order Help</Text>
+        </View>
+      </SafeAreaView>
     </View>
-  )
-}
+  );
+};
 
 export default DeliveryScreen
