cedd22d3aae10b49c5e689065e98033f1bae5afd
Uche Chris <chrisuche58@yahoo.com>
2023-05-11 22:35:56 +0100

    update

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -1,9 +1,13 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { useSelector } from 'react-redux';
 import { selectRestaurant } from '../features/restaurantSlice';
 import { XCircleIcon, XMarkIcon } from 'react-native-heroicons/solid';
+import * as Progress from 'react-native-progress';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
+
 
 
 const DeliveryScreen = () => {
@@ -20,9 +24,24 @@ const DeliveryScreen = () => {
         </View>
 
         <View className="bg-white mx-5 my-2 rounded-md p-6 z-50 shadow-md">
-            <Text className= "text-lg text-gray-400">Estimated Arrival</Text>
-            <Text className= "text-4xl font-bold">20-35 Min</Text>
-        </View>
+          <View className="flex-row justify-between">
+            <View>
+                <Text className= "text-lg text-gray-400">Estimated Arrival</Text>
+                <Text className= "text-4xl font-bold">20-35 Min</Text>
+            </View>
+            <Image 
+                source={{
+                    uri:"https://links.papareact.com/fls",
+                }}
+                className=" h-28 w-28"
+                />
+          </View>
+          <Progress.Bar size={30} color="#00CCBB" indeterminate={true}/>
+
+          <Text className="mt-3 text-gray-500">
+            Your Order From {restaurant.title} is being prepared
+          </Text>
+         </View>
       </SafeAreaView>
     </View>
   );
