f27ed24e8336ef28743c3dcc6d2d023bec3e8219
Uche Chris <chrisuche58@yahoo.com>
2023-05-11 14:31:58 +0100

    Estimated Arrival

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -11,12 +11,17 @@ const DeliveryScreen = () => {
     const restaurant = useSelector(selectRestaurant);
   return (
     <View className="bg-[#00CCBB] flex-1">
-      <SafeAreaView>
-        <View >
+      <SafeAreaView className="z-50">
+        <View className="flex-row justify-between items-center p-5">
             <TouchableOpacity onPress={() => navigation.navigate("Home")}>
                 <XCircleIcon color="white" size={30}/>
             </TouchableOpacity>
-            <Text>Order Help</Text>
+            <Text  className="font-light text-white text-lg">Order Help</Text>
+        </View>
+
+        <View className="bg-white mx-5 my-2 rounded-md p-6 z-50 shadow-md">
+            <Text className= "text-lg text-gray-400">Estimated Arrival</Text>
+            <Text className= "text-4xl font-bold">20-35 Min</Text>
         </View>
       </SafeAreaView>
     </View>
